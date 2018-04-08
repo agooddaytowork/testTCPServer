0b1d9c465b5fe2bfce70d6da3cbdbda1567cb3e6
tamtam <tam.duong@ascenx.com>
2018-04-08 10:59:57 +0700

    so noisy

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <QCoreApplication>
 #include "fountainserver.h"
 #include "simpleserialinterface.h"
+#include <QSerialPortInfo>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     fountainServer aServer;
+
+    foreach (QSerialPortInfo port, QSerialPortInfo::availablePorts()) {
+        qDebug() << port.portName();
+    }
     return a.exec();
 }
